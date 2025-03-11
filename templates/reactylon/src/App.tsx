39efb71aa66db900bc059e92e949c95fd2bd3321
Simone De Vittorio <simonedevittorio.me@gmail.com>
2025-03-11 13:38:45 +0100

    chore: add physics + bump version

--- templates/reactylon/src/App.tsx
+++ templates/reactylon/src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { Engine } from 'reactylon/web';
 import { Scene } from 'reactylon';
 import Content from './Content';
+import { HavokPlugin } from '@babylonjs/core';
 
-const App: React.FC = () => {
+type AppProps = {
+    havok: unknown;
+}
+
+const App: React.FC<AppProps> = ({ havok }) => {
 
     return (
         <Engine antialias>
-            <Scene onSceneReady={scene => scene.createDefaultCameraOrLight(true, undefined, true)}>
+            <Scene onSceneReady={scene => scene.createDefaultCameraOrLight(true, undefined, true)} physicsOptions={{
+                plugin: new HavokPlugin(true, havok)
+            }}>
                 <Content />
             </Scene>
         </Engine>
